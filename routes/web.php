9d1adaceed055a45da8b651045cc5729ab6c14a3
Emerson <emersonlucenaoficial@gmail.com>
2021-10-14 18:25:20 -0300

    adicionado middleware de autenticaçao

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Site\SiteController;
 use App\Http\Controllers\Painel\ProdutoController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -61,20 +62,30 @@ Route::get('/empresa', function() {
 });
 */
 
-Route::get('/painel/produtos/tests', [ProdutoController::class, 'tests']);
-Route::get('/painel/produtos', [ProdutoController::class, 'index'])->name('listaProdutos');
-Route::get('/painel/produtos/create', [ProdutoController::class, 'create'])->name('criacaoDeProdutos');
-Route::post('/painel/produtos/', [ProdutoController::class, 'store'])->name('envioFormProdutos');
-Route::get('/painel/produtos/{id}/edit', [ProdutoController::class, 'edit'])->name('editaProdutos');
-Route::put('/painel/produtos/{id}', [ProdutoController::class, 'update'])->name('atualizaProdutos');
-Route::get('/painel/produtos/{id}', [ProdutoController::class, 'show'])->name('listaProduto');
-Route::delete('/painel/produtos/{id}', [ProdutoController::class, 'destroy'])->name('deletaProduto');
+Route::middleware('auth')->group(function () {
+    Route::get('/painel/produtos/tests', [ProdutoController::class, 'tests']);
+    Route::get('/painel/produtos', [ProdutoController::class, 'index'])->name('listaProdutos');
+    Route::get('/painel/produtos/create', [ProdutoController::class, 'create'])->name('criacaoDeProdutos');
+    Route::post('/painel/produtos/', [ProdutoController::class, 'store'])->name('envioFormProdutos');
+    Route::get('/painel/produtos/{id}/edit', [ProdutoController::class, 'edit'])->name('editaProdutos');
+    Route::put('/painel/produtos/{id}', [ProdutoController::class, 'update'])->name('atualizaProdutos');
+    Route::get('/painel/produtos/{id}', [ProdutoController::class, 'show'])->name('listaProduto');
+    Route::delete('/painel/produtos/{id}', [ProdutoController::class, 'destroy'])->name('deletaProduto');
 
-Route::group([], function() {
-    Route::get('/categoriaop/{id?}', [SiteController::class, 'categoriaop']);
-    Route::get('/categoria/{id}', [SiteController::class, 'categoria']);
-    Route::get('/', [SiteController::class, 'index']);
-    Route::get('/contato', [SiteController::class, 'contato']);
+    Route::group([], function () {
+        Route::get('/categoriaop/{id?}', [SiteController::class, 'categoriaop']);
+        Route::get('/categoria/{id}', [SiteController::class, 'categoria']);
+        Route::get('/', [SiteController::class, 'index']);
+        Route::get('/contato', [SiteController::class, 'contato']);
+    });
 });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
